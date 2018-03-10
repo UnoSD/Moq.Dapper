f5fcfb0ef7b9fab3857120aaccf3eb3c3f09340f
Ross Ellerington <ross.ellerington@tredsoft.co.uk>
2018-03-10 15:46:34 +0000

    Added Support for Async nullables (but failing test)

--- Moq.Dapper/DbConnectionMockExtensions.cs
+++ Moq.Dapper/DbConnectionMockExtensions.cs
@@ -71,21 +71,34 @@ namespace Moq.Dapper
             }
             else
             {
-                var properties =
-                        type.GetProperties().
-                             Where
-                             (
-                              info => info.CanRead &&
-                                      (info.PropertyType.IsPrimitive ||
-                                       info.PropertyType == typeof(DateTime) ||
-                                       info.PropertyType == typeof(DateTimeOffset) ||
-                                       info.PropertyType == typeof(decimal) ||
-                                       info.PropertyType == typeof(Guid) ||
-                                       info.PropertyType == typeof(string) ||
-                                       info.PropertyType == typeof(TimeSpan))).
-                             ToList();
+                bool IsNullable(Type t) =>
+                    t.IsGenericType &&
+                    t.GetGenericTypeDefinition() == typeof(Nullable<>);
 
-                var columns = properties.Select(property => new DataColumn(property.Name, property.PropertyType)).ToArray();
+                Type GetDataColumnType(Type source) =>
+                    IsNullable(source) ?
+                        Nullable.GetUnderlyingType(source) :
+                        source;
+
+                bool IsMatchingType(Type t) =>
+                    t.IsPrimitive ||
+                    t == typeof(DateTime) ||
+                    t == typeof(DateTimeOffset) ||
+                    t == typeof(decimal) ||
+                    t == typeof(Guid) ||
+                    t == typeof(string) ||
+                    t == typeof(TimeSpan);
+
+                var properties =
+                    type.GetProperties().Where(info => info.CanRead &&
+                                                       IsMatchingType(info.PropertyType) ||
+                                                       IsNullable(info.PropertyType) &&
+                                                       IsMatchingType(Nullable.GetUnderlyingType(info.PropertyType)))
+                        .ToList();
+
+                var columns = properties.Select(property =>
+                        new DataColumn(property.Name, GetDataColumnType(property.PropertyType)))
+                    .ToArray();
 
                 dataTable.Columns.AddRange(columns);
 
