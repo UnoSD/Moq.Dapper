d372f1600ae63d6b2883d2a4a6df61ec6f069dd4
Ross Ellerington <ross.ellerington@tredsoft.co.uk>
2018-06-21 17:39:43 +0100

    Added Support for Async nullables (#11)
    * Added Support for Async nullables

--- Moq.Dapper.Test/DapperTest.cs
+++ Moq.Dapper.Test/DapperTest.cs
@@ -2,6 +2,7 @@
 using System.Data;
 using System.Data.Common;
 using System.Linq;
+using System.Threading.Tasks;
 using Dapper;
 using NUnit.Framework;
 using NUnit.Framework.Constraints;
@@ -114,6 +115,64 @@ namespace Moq.Dapper.Test
                       .Returns(expected);
 
             var actual = connection.Object.Query<ComplexType>("", null, null, true, null, null).ToList();
+
+            Assert.That(actual.Count, Is.EqualTo(expected.Length));
+
+            foreach (var complexObject in expected)
+            {
+                var match = actual.Where(co => co.StringProperty == complexObject.StringProperty &&
+                                               co.IntegerProperty == complexObject.IntegerProperty &&
+                                               co.GuidProperty == complexObject.GuidProperty &&
+                                               co.DateTimeProperty == complexObject.DateTimeProperty &&
+                                               co.NullableIntegerProperty == complexObject.NullableIntegerProperty &&
+                                               co.NullableDateTimeProperty == complexObject.NullableDateTimeProperty);
+
+                Assert.That(match.Count, Is.EqualTo(1));
+            }
+        }
+
+        [Test]
+        public void QueryAsyncGenericComplexType()
+        {
+            var connection = new Mock<DbConnection>();
+
+            var expected = new[]
+            {
+                new ComplexType
+                {
+                    StringProperty = "String1",
+                    IntegerProperty = 7,
+                    GuidProperty = Guid.Parse("CF01F32D-A55B-4C4A-9B33-AAC1C20A85BB"),
+                    DateTimeProperty = new DateTime(2000, 1, 1),
+                    NullableDateTimeProperty = new DateTime(2000, 1, 1),
+                    NullableIntegerProperty = 9
+                },
+                new ComplexType
+                {
+                    StringProperty = "String2",
+                    IntegerProperty = 77,
+                    GuidProperty = Guid.Parse("FBECE122-6E2E-4791-B781-C30843DFE343"),
+                    DateTimeProperty = new DateTime(2000, 1, 2),
+                    NullableDateTimeProperty = new DateTime(2000, 1, 2),
+                    NullableIntegerProperty = 99
+                },
+                new ComplexType
+                {
+                    StringProperty = "String3",
+                    IntegerProperty = 777,
+                    GuidProperty = Guid.Parse("712B6DA1-71D8-4D60-8FEF-3F4800A6B04F"),
+                    DateTimeProperty = new DateTime(2000, 1, 3),
+                    NullableDateTimeProperty = null,
+                    NullableIntegerProperty = null
+                }
+            };
+
+            connection.SetupDapperAsync(c => c.QueryAsync<ComplexType>(It.IsAny<string>(), null, null, null, null))
+                      .ReturnsAsync(expected);
+
+            var actual = connection.Object
+                                   .QueryAsync<ComplexType>("", null, null, null, null)
+                                   .GetAwaiter().GetResult().ToList();
 
             Assert.That(actual.Count, Is.EqualTo(expected.Length));
 
--- Moq.Dapper/DbDataReaderFactory.cs
+++ Moq.Dapper/DbDataReaderFactory.cs
@@ -27,19 +27,33 @@ namespace Moq.Dapper
             }
             else
             {
+                bool IsNullable(Type t) =>
+                    t.IsGenericType &&
+                    t.GetGenericTypeDefinition() == typeof(Nullable<>);
+
+                Type GetDataColumnType(Type source) =>
+                    IsNullable(source) ?
+                        Nullable.GetUnderlyingType(source) :
+                        source;
+
+                bool IsMatchingType(Type t) =>
+                    t.IsPrimitive ||
+                    t == typeof(DateTime) ||
+                    t == typeof(DateTimeOffset) ||
+                    t == typeof(decimal) ||
+                    t == typeof(Guid) ||
+                    t == typeof(string) ||
+                    t == typeof(TimeSpan);
+
                 var properties =
                     type.GetProperties()
                         .Where(info => info.CanRead &&
-                                      (info.PropertyType.IsPrimitive ||
-                                       info.PropertyType == typeof(DateTime) ||
-                                       info.PropertyType == typeof(DateTimeOffset) ||
-                                       info.PropertyType == typeof(decimal) ||
-                                       info.PropertyType == typeof(Guid) ||
-                                       info.PropertyType == typeof(string) ||
-                                       info.PropertyType == typeof(TimeSpan)))
+                                       IsMatchingType(info.PropertyType) ||
+                                       IsNullable(info.PropertyType) &&
+                                       IsMatchingType(Nullable.GetUnderlyingType(info.PropertyType)))
                         .ToList();
 
-                var columns = properties.Select(property => new DataColumn(property.Name, property.PropertyType)).ToArray();
+                var columns = properties.Select(property => new DataColumn(property.Name, GetDataColumnType(property.PropertyType))).ToArray();
 
                 dataTable.Columns.AddRange(columns);
 
