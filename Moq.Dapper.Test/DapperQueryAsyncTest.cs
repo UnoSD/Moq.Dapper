a9f9e1fda9b693c8284a28af0c67d620440a72c1
UnoSD <unosd.dev@gmail.com>
2019-07-13 07:50:37 +0000

    More

--- Moq.Dapper.Test/DapperQueryAsyncTest.cs
+++ Moq.Dapper.Test/DapperQueryAsyncTest.cs
@@ -105,16 +105,16 @@ namespace Moq.Dapper.Test
                 Assert.That(match.Count, Is.EqualTo(1));
             }
         }
-    }
 
-    public class ComplexType
-    {
-        public int IntegerProperty { get; set; }
-        public string StringProperty { get; set; }
-        public Guid GuidProperty { get; set; }
-        public DateTime DateTimeProperty { get; set; }
-        public DateTime? NullableDateTimeProperty { get; set; }
-        public int? NullableIntegerProperty { get; set; }
-        public byte[] ByteArrayPropery { get; set; }
+        public class ComplexType
+        {
+            public int IntegerProperty { get; set; }
+            public string StringProperty { get; set; }
+            public Guid GuidProperty { get; set; }
+            public DateTime DateTimeProperty { get; set; }
+            public DateTime? NullableDateTimeProperty { get; set; }
+            public int? NullableIntegerProperty { get; set; }
+            public byte[] ByteArrayPropery { get; set; }
+        }
     }
 }
