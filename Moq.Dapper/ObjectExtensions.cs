c3a87e43f7db2f7920612a897db3e6c574191a54
Tim VanFosson <tim.vanfosson@imaginelearning.com>
2022-07-06 17:42:09 -0500

    Adds implementation that handles null returns for default queries

--- Moq.Dapper/ObjectExtensions.cs
+++ Moq.Dapper/ObjectExtensions.cs
@@ -7,9 +7,17 @@ namespace Moq.Dapper
 {
     static class ObjectExtensions
     {
-         internal static DataTable ToDataTable(this object result, Type resultType) =>
-             result is IEnumerable results ?
-             results.ToDataTable(resultType.GenericTypeArguments.Single()) :
-             new[] { result }.ToDataTable(resultType);
+        internal static DataTable ToDataTable(this object result, Type resultType)
+        {
+            switch (result)
+            {
+                case null:
+                    return Array.CreateInstance(resultType, 0).ToDataTable(resultType);
+                case IEnumerable results:
+                    return results.ToDataTable(resultType.GenericTypeArguments.Single());
+                default:
+                    return new[] { result }.ToDataTable(resultType);
+            }
+        }
     }
 }
